b38d0fce99e9e7658960a7508d8094be58d0f459
CARNIFEXIUM\Carnifex <carnifex.za@gmail.com>
2018-11-15 17:13:00 +0200

    Hidden a test button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -162,6 +162,7 @@
             this.button1.TabIndex = 8;
             this.button1.Text = "TEST";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar1
